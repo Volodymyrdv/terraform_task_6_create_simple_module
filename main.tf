dd35fc83a1a479f460787521ab9cab5ab39a1045
Volodymyrdv <vovakryt123453@gmail.com>
2025-07-01 21:36:37 +0300

    Solution

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.34.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = true
+    }
+  }
+}
+
+module "resource_group_storage" {
+  source               = "Volodymyrdv/resource_group_storage/azurerm"
+  version              = "1.0.0"
+  location             = "Poland Central"
+  resource_group_name  = "rg-storage"
+  storage_account_name = "valodya33"
+}
